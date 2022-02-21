b9d18efab3557047ac912468eb258f1f07342925
a19simma <a19simma@student.his.se>
2022-02-21 16:34:25 +0100

    changed scenario

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from stable_baselines3 import PPO
 # Basics methods for the vizdoom environment are:
 # make_action which takes a list of button states given by an array of 0 or 1 with the 
 # length of the number of buttons.
-#
 
-LOG_DIR = '../vizdoomlog/logs'
+LOG_DIR = '../vizdoomlog/defendcenter'
 
-env = VizDoomTrain('basic')
+env = VizDoomTrain('defend_the_center')
+
 callback = TrainCallback(10000)
 
 model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=0.0001, n_steps=2048)
 
-model.learn(total_timesteps=10000, callback=callback)
+model.learn(total_timesteps=70000, callback=callback)
