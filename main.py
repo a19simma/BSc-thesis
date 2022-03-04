e2f97f46e6c483ba41a0dd04b5261a2ddba75cb3
a19simma <a19simma@student.his.se>
2022-03-04 13:29:12 +0100

    naming runs complete

--- main.py
+++ main.py
@@ -12,24 +12,24 @@ from stable_baselines3.common.monitor import Monitor
 
 SCENARIO = 'defend_the_center'
 LOG_DIR = 'logs/' + SCENARIO
-RUN_NAME = '42x42_schulman_standard_hp_10x2048NSTEP_stepsize3e-4'
 
 #schulman PPO paper from 2017 parameters GAE params and Discount is 
 # not changed. The stable baselines3 PPO implementation is using the same
 # defaults as in the paper's Mujoco experiment.
 
 model_params = {
-    'n_steps': 2048*10,
-    'learning_rate': 3e-4,
+    'n_steps': 2048,
+    'learning_rate': 1e-2,
     'n_epochs': 10,
     #'gamma': 0.99,
     #'gae_lambda': 0.95,
     'batch_size': 64,
 }
 
-RUN_NAME = ""
-for x in model_params:
-    RUN_NAME + x
+RUN_NAME = ''
+for key in model_params:
+    RUN_NAME += key + '=' + str(model_params[key]) + '_'
+RUN_NAME = RUN_NAME[:-1]
 
 env = VizDoomTrain(SCENARIO)
 env = Monitor(env, (LOG_DIR + '/' + RUN_NAME))
