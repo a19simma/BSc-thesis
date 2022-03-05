adc8867eb202ca7d7705c4915b68d05ddf5c1355
a19simma <a19simma@student.his.se>
2022-03-05 14:44:59 +0100

    Bayesian Optimization https://github.com/fmfn/BayesianOptimization implemented, test runs ongoing

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from callback import TrainCallback
 from stable_baselines3 import DQN
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.monitor import Monitor
+from bayes_opt import BayesianOptimization
+from bayes_opt.logger import JSONLogger
+from bayes_opt.event import Events
 
 # Basics methods for the vizdoom environment are:
 # make_action which takes a list of button states given by an array of 0 or 1 with the 
@@ -10,28 +13,53 @@ from stable_baselines3.common.monitor import Monitor
 
 SCENARIO = 'deadly_corridor'
 LOG_DIR = 'logs/' + SCENARIO
+TOTAL_TIMESTEPS = 20000
 
-#Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
-model_params = {
-        'learning_rate': 1e-4,
-        'buffer_size':  int(5e5), # size of the buffer because of ram limitations.
-        'learning_starts': 50000,
-        'batch_size': 32,
-        'tau': 1.0,
-        'gamma': 0.99,
-        'train_freq': 4,
-        'gradient_steps': 1,
+model_params_bounds = {
+    'learning_rate': (1e-1,1e-7),
+    'batch_size': (8,128),
+    'train_freq': (1,50),
+    'gradient_steps': (-1,10),
 }
 
-RUN_NAME = ''
-for key in model_params:
-    RUN_NAME += key + '=' + str(model_params[key]) + '_'
-RUN_NAME = RUN_NAME[:-1]
+def train_model(**model_params):
+    
+    #Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
+    model_params = {
+        'learning_rate': model_params['learning_rate'],
+        'buffer_size':  int(5e5), # size of the buffer because of ram limitations.
+        'learning_starts': TOTAL_TIMESTEPS/20,
+        'batch_size': int(model_params['batch_size']),
+        'tau': 1.0,
+        'gamma': 0.99,
+        'train_freq': int(model_params['train_freq']),
+        'gradient_steps': int(model_params['gradient_steps']),
+    }
 
-env = VizDoomTrain(SCENARIO)
-env = Monitor(env)
-model = DQN('CnnPolicy', env, verbose=1, **model_params)
-logger = configure(LOG_DIR + '/' + RUN_NAME, ["stdout", "csv", "tensorboard"])
-model.set_logger(logger)
-callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
-model.learn(total_timesteps=300000, callback=callback, log_interval=512)
+    RUN_NAME = ''
+    for key in model_params:
+        RUN_NAME += key + '=' + str(model_params[key]) + '_'
+        RUN_NAME = RUN_NAME[:-1]
+
+    env = VizDoomTrain(SCENARIO)
+    env = Monitor(env)
+    model = DQN('CnnPolicy', env, verbose=1, **model_params)
+    logger = configure(LOG_DIR + '/' + RUN_NAME, ["stdout", "csv", "tensorboard"])
+    model.set_logger(logger)
+    callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
+    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=512)
+
+optimizer = BayesianOptimization(
+    f=train_model,
+    pbounds=model_params_bounds,
+    random_state=1,
+)
+optlogger = JSONLogger(path="./logs.json")
+optimizer.subscribe(Events.OPTIMIZATION_STEP, optlogger)
+
+optimizer.maximize(
+    init_points=2,
+    n_iter=20,
+)
+
+print(optimizer.max)
