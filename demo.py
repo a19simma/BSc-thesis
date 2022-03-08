a96d7752250e02bd29e45c217c3b44b94f784898
a19simma <a19simma@student.his.se>
2022-03-08 17:41:21 +0100

    demo file

---
+++ demo.py
@@ -0,0 +1,30 @@
+from vizdoomEnv import VizDoomTrain
+from stable_baselines3 import DQN, A2C, PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.evaluation import evaluate_policy
+import main
+import fnmatch
+import os
+import time
+import numpy as np
+
+DIR = "./logs/" + main.STUDY_NAME
+TRIAL_N = 0
+
+env = VizDoomTrain(main.SCENARIO, visible=True)
+env = Monitor(env)
+model_dir = fnmatch.filter(os.listdir(DIR), "Trial_" + str(TRIAL_N) + "*")[0]
+
+model = DQN.load(DIR + "/" + model_dir + "/models/best_model_290000")
+for episode in range(10):
+    obs = env.reset()
+    done = False
+    total_reward = 0
+    while not done:
+        obs = np.array(obs)
+        obs = obs.reshape((-1,) + env.observation_space.shape)
+        action = model.predict(obs)[0]
+        obs, reward, done, info = env.step(action[0])
+        time.sleep(0.04)
+        total_reward += reward
+    print("episode finished with a score of: " + str(total_reward))
