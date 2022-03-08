f254bdb7f8528ebf3dca6d6894570e5251d2019c
a19simma <a19simma@student.his.se>
2022-03-08 17:39:50 +0100

    demo program, loading a trained model and running 10 episodes

--- main.py
+++ main.py
@@ -7,52 +7,59 @@ from stable_baselines3.common.evaluation import evaluate_policy
 import optuna
 
 # Basics methods for the vizdoom environment are:
-# make_action which takes a list of button states given by an array of 0 or 1 with the 
+# make_action which takes a list of button states given by an array of 0 or 1 with the
 # length of the number of buttons.
 
 SCENARIO = 'deadly_corridor'
-LOG_DIR = 'logs/' + SCENARIO
 TOTAL_TIMESTEPS = 3e5
+ALGORITHM = "DQN"
+STUDY_NAME = SCENARIO + "_" + ALGORITHM
+LOG_DIR = 'logs/' + STUDY_NAME
 
-#Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
+# Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
+
+
 def optimize_ppo(trial):
     return{
-        'learning_rate': trial.suggest_loguniform('learning_rate',1e-7,1),
-        'buffer_size':  int(1e5), # size of the buffer was reduced because of ram limitations.
-        'learning_starts': TOTAL_TIMESTEPS/20,
-        'batch_size': trial.suggest_int('batch_size',1,128),
-        'tau': 1.0,
-        'gamma': 0.99,
-        'train_freq': trial.suggest_int('train_freq',1,50),
-        'gradient_steps': trial.suggest_int('gradient_steps',1,10),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-7, 1),
+        # size of the buffer was reduced because of ram limitations.
+        'buffer_size':  int(1e5),
+        'learning_starts': trial.suggest_int('learning_starts', TOTAL_TIMESTEPS/100, TOTAL_TIMESTEPS/2),
+        'batch_size': trial.suggest_int('batch_size', 1, 128),
+        'tau': trial.suggest_float('tau', 0, 1.0),
+        'gamma': trial.suggest_float('gamma', 0, 1.0),
+        'train_freq': trial.suggest_int('train_freq', 1, 2048),
+        'gradient_steps': trial.suggest_int('gradient_steps', 1, 10),
     }
 
+
 def optimize_agent(trial):
-    model_params = optimize_ppo(trial) 
+    model_params = optimize_ppo(trial)
     RUN_NAME = 'Trial_' + str(trial.number) + '_'
     for key in model_params:
         RUN_NAME += key + '=' + str(model_params[key]) + '_'
-        RUN_NAME = RUN_NAME[:-1]   
+        RUN_NAME = RUN_NAME[:-1]
 
     env = VizDoomTrain(SCENARIO)
     env = Monitor(env)
-    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, **model_params)
-    logger = configure(LOG_DIR + '/' + RUN_NAME, ["stdout", "csv", "tensorboard"])
+    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR,
+                verbose=0, **model_params)
+    logger = configure(LOG_DIR + '/' + RUN_NAME,
+                       ["stdout", "csv", "tensorboard"])
     model.set_logger(logger)
     callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
-    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=256) #decrease frequency of output with log_interval
+    # decrease frequency of output with log_interval
+    model.learn(total_timesteps=TOTAL_TIMESTEPS,
+                callback=callback, log_interval=256)
     mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
     return mean_reward
 
+
 if __name__ == '__main__':
     # for the distributed solution a mysql server is needed with a database names optuna
-    STUDY_NAME = SCENARIO + "_PPO"
+    study = optuna.create_study(direction='maximize', study_name=STUDY_NAME,
+                                storage="mysql://root@localhost:3306/optuna", load_if_exists=True)
     try:
-        study = optuna.load_study(study_name=STUDY_NAME, storage="mysql://root@localhost:3306/optuna")
-    except KeyError:
-        print('Could not find study, creating...')
-        study = optuna.create_study(direction='maximize', study_name=STUDY_NAME, storage="mysql://root@localhost:3306/optuna")
-    try:
-        study.optimize(optimize_agent, n_trials=10, gc_after_trial=True)
+        study.optimize(optimize_agent, n_trials=3, gc_after_trial=True)
     except KeyboardInterrupt:
         print('Interrupted by keyboard')
--- vizdoomEnv.py
+++ vizdoomEnv.py
@@ -6,20 +6,22 @@ import os.path
 
 
 # Basics methods for the vizdoom environment are:
-# make_action which takes a list of button states given by an array of 0 or 1 with the 
+# make_action which takes a list of button states given by an array of 0 or 1 with the
 # length of the number of buttons.
 #
 class VizDoomTrain(Env):
-    def __init__(self, scenario):
+    def __init__(self, scenario, visible=False):
         super().__init__()
-        self.game = DoomGame() #type: ignore
+        self.game = DoomGame()  # type: ignore
         config = os.path.join(scenarios_path, scenario + '.cfg')
         self.game.load_config(config)
-        self.game.set_window_visible(False)
-        self.game.set_screen_format(ScreenFormat.GRAY8) #type: ignore
-        self.game.set_screen_resolution(ScreenResolution.RES_160X120) #type: ignore
+        self.game.set_window_visible(visible)
+        self.game.set_screen_format(ScreenFormat.GRAY8)  # type: ignore
+        self.game.set_screen_resolution(
+            ScreenResolution.RES_160X120)  # type: ignore
         self.game.init()
-        self.observation_space = Box(low=0, high=255, shape=(1, 120, 160), dtype=np.uint8)
+        self.observation_space = Box(
+            low=0, high=255, shape=(1, 120, 160), dtype=np.uint8)
         self.action_space = Discrete(self.game.get_available_buttons_size())
 
     def step(self, action):
@@ -27,14 +29,14 @@ class VizDoomTrain(Env):
         actions = np.identity(buttons)
         # second argument is the number of skipped tics, could be interesting to tweak.
         reward = self.game.make_action(actions[action], 4)
-        if self.game.get_state(): 
+        if self.game.get_state():
             ammo = self.game.get_state().game_variables[0]
             info = ammo
             state = self.game.get_state().screen_buffer
         else:
             state = np.zeros(self.observation_space.shape)
-            info = 0 
-        info = {"info":info}
+            info = 0
+        info = {"info": info}
         done = self.game.is_episode_finished()
         return state, reward, done, info
 
@@ -42,10 +44,12 @@ class VizDoomTrain(Env):
         self.game.new_episode()
         state = self.game.get_state().screen_buffer
         return state
-    
+
+    def render(self):
+        pass
+
     def getReward(self):
         return self.game.get_total_reward()
 
     def close(self):
         self.game.close()
-
