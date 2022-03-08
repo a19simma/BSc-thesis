553a758e692d542c156b538baa7afd774c706283
a19simma <a19simma@student.his.se>
2022-03-08 02:31:37 +0100

    adapted to DQN

--- main.py
+++ main.py
@@ -15,18 +15,17 @@ LOG_DIR = 'logs/' + SCENARIO
 TOTAL_TIMESTEPS = 3e5
 
 
-#Defaults are taken from the 2017 paper by schulman et al. https://arxiv.org/abs/1707.06347
+#Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
 def optimize_ppo(trial):
     return{
-        #'n_steps': int(trial.suggest_loguniform('n_steps', 16, 2048)),                     Fungerar
-        'gamma': trial.suggest_loguniform('gamma', 0.9, 0.9999),#                           Fungerar
-        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-5, 1.),#              Fungerar
-        'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 1e-1),#                      Fungerar
-        #'batch_size' : int(trial.suggest_loguniform('batch_size', 1, n_steps * n_envs)),   Fungerar, men vi behöver den nog inte
-        'n_epochs': int(trial.suggest_loguniform('n_epochs', 1, 48))#,                      Fungerar
-        #'cliprange': trial.suggest_uniform('cliprange', 0.1, 0.4),                         Fungerar inte
-        #'noptepochs': int(trial.suggest_loguniform('noptepochs', 1, 48)),                  Fungerar inte
-        #'lam': trial.suggest_uniform('lam', 0.8, 1.)                                       Fungerar inte
+        'learning_rate': trial.suggest_loguniform('learning_rate',1e-7,1),
+        'buffer_size':  int(1e5), # size of the buffer was reduced because of ram limitations.
+        'learning_starts': TOTAL_TIMESTEPS/20,
+        'batch_size': trial.suggest_int('batch_size',1,128),
+        'tau': 1.0,
+        'gamma': 0.99,
+        'train_freq': trial.suggest_int('train_freq',1,50),
+        'gradient_steps': trial.suggest_int('gradient_steps',1,10),
     }
 
 def optimize_agent(trial):
@@ -38,11 +37,11 @@ def optimize_agent(trial):
 
     env = VizDoomTrain(SCENARIO)
     env = Monitor(env)
-    model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, n_steps=2048, verbose=0, **model_params)
+    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, **model_params)
     logger = configure(LOG_DIR + '/' + RUN_NAME, ["stdout", "csv", "tensorboard"])
     model.set_logger(logger)
     callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
-    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=1) #decrease frequency of output with log_interval
+    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=256) #decrease frequency of output with log_interval
     mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
     return mean_reward
 
