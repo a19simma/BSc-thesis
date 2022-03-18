6ff0fd45b041d83685a6ea79c2e8e106838079c0
e15marol <markus-olsson@outlook.com>
2022-03-18 14:49:24 +0100

    Just saving settings

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ sys.setrecursionlimit(800)
 
 
 SCENARIO = 'deadly_corridor'
-LOG_DIR = 'logs/' + SCENARIO
+LOG_DIR = 'logs/' + SCENARIO + '_PPO_TEST'
 
 
 
 def optimize_ppo(trial):
     return{
         #'n_steps': int(trial.suggest_loguniform('n_steps', 16, 2048)),                     Fungerar
-        'gamma': trial.suggest_loguniform('gamma', 0.9, 0.9999),#                           Fungerar
-        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-5, 1.),#              Fungerar
-        'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 1e-1),#                      Fungerar
+        #'gamma': trial.suggest_loguniform('gamma', 0.9, 0.9999),#                           Fungerar
+        #'learning_rate': trial.suggest_loguniform('learning_rate', 1e-5, 1.),#              Fungerar
+        #'ent_coef': trial.suggest_loguniform('ent_coef', 1e-8, 1e-1),#                      Fungerar
         #'batch_size' : int(trial.suggest_loguniform('batch_size', 1, n_steps * n_envs)),   Fungerar, men vi behöver den nog inte
-        'n_epochs': int(trial.suggest_loguniform('n_epochs', 1, 48))#,                      Fungerar
+        #'n_epochs': int(trial.suggest_loguniform('n_epochs', 1, 48))#,                      Fungerar
         #'cliprange': trial.suggest_uniform('cliprange', 0.1, 0.4),                         Fungerar inte
         #'noptepochs': int(trial.suggest_loguniform('noptepochs', 1, 48)),                  Fungerar inte
         #'lam': trial.suggest_uniform('lam', 0.8, 1.)                                       Fungerar inte
@@ -43,14 +43,17 @@ def optimize_agent(trial):
 
     env = VizDoomTrain(SCENARIO)
     env = Monitor(env, (LOG_DIR + '/' + RUN_NAME))
-    model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, n_steps=2048, verbose=1, **model_params)
+    #model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, n_steps=2048, verbose=1, **model_params)
+    #model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, learning_rate=0.0001, n_steps=2048, verbose=1, **model_params)
+    model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, learning_rate=0.0001, n_steps=2048, clip_range=.1, gamma=.95, gae_lambda=.9, verbose=1, **model_params)
+    
     logger = configure(LOG_DIR + '/' + RUN_NAME, ["stdout", "csv", "tensorboard"])
     model.set_logger(logger)
     callback = TrainCallback(50000, LOG_DIR + '/' + RUN_NAME)
-    model.learn(total_timesteps=30000, callback=callback)
+    model.learn(total_timesteps=500000, callback=callback)
 
-    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10) 
-    return -1 * mean_reward
+    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=3) 
+    return mean_reward
 
 
 if __name__ == '__main__':
--- vizdoomEnv.py
+++ vizdoomEnv.py
@@ -15,24 +15,46 @@ class VizDoomTrain(Env):
         path = 'vizdoom/scenarios/' + scenario + '.cfg'
         self.game = DoomGame() #type: ignore
         self.game.load_config(path)
-        self.game.set_window_visible(False)
+        self.game.set_window_visible(True)
         self.game.set_screen_format(ScreenFormat.GRAY8) #type: ignore
         self.game.set_screen_resolution(ScreenResolution.RES_160X120) #type: ignore
         self.game.init()
         self.observation_space = Box(low=0, high=255, shape=(1, 120, 160), dtype=np.uint8)
         self.action_space = Discrete(self.game.get_available_buttons_size())
 
+        #Game variables: HEALTH DAMAGE_TAKEN HITCOUNT SELECTED_WEAPON_AMMO
+        self.damage_taken = 0
+        self.hitcount = 0
+        self.ammo = 26
+
+
     def step(self, action):
         buttons = self.game.get_available_buttons_size()
         actions = np.identity(buttons)
         # second argument is the number of skipped tics, could be interesting to tweak.
-        reward = self.game.make_action(actions[action], 4)
+        movement_reward = self.game.make_action(actions[action], 4)
 
+        reward = 0
         #print(self.game.get_available_game_variables())
         if self.game.get_state(): 
-            ammo = self.game.get_state().game_variables[0]
-            info = ammo
+            
             state = self.game.get_state().screen_buffer
+
+            # Reward shaping
+            game_variables = self.game.get_state().game_variables
+            health, damage_taken, hitcount, ammo = game_variables
+
+            # Calculate reward deltas (changes)
+            damage_taken_delta = -damage_taken + self.damage_taken
+            self.damage_taken = damage_taken
+            hitcount_delta = hitcount - self.hitcount
+            self.hitcount = hitcount
+            ammo_delta = ammo - self.ammo
+            self.ammo = ammo
+
+            reward = movement_reward + damage_taken_delta*10 + hitcount_delta*200  + ammo_delta*5 
+            
+            info = ammo
         else:
             state = np.zeros(self.observation_space.shape)
             info = 0 
