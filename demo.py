47d9759299aecc02f6c422ffa391588fbe57747e
a19simma <a19simma@student.his.se>
2022-03-18 12:17:14 +0100

    file selector with tkinter

--- demo.py
+++ demo.py
@@ -3,19 +3,21 @@ from stable_baselines3 import DQN, A2C, PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
 import main
-import fnmatch
 import os
 import time
 import numpy as np
+import tkinter
+from tkinter import filedialog
 
 DIR = "./logs/" + main.STUDY_NAME
 TRIAL_N = 0
 
 env = VizDoomTrain(main.SCENARIO, visible=True)
 env = Monitor(env)
-model_dir = fnmatch.filter(os.listdir(DIR), "Trial_" + str(TRIAL_N) + "*")[0]
+tkinter.Tk().withdraw()
+model_dir = filedialog.askopenfilename()
 
-model = DQN.load(DIR + "/" + model_dir + "/models/best_model_290000")
+model = DQN.load(model_dir)
 for episode in range(10):
     obs = env.reset()
     done = False
