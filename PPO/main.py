1bc13e1fdaf9ff1e3d782a29213335945d425cec
e15marol <markus-olsson@outlook.com>
2022-03-19 21:12:35 +0100

    Reward shaping fixed
    Reward shaping works properly now, could use 1 million steps to see how it performs.

--- PPO/main.py
+++ PPO/main.py
@@ -1,24 +1,24 @@
 import os, sys
 sys.path.append('../')
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from vizdoomEnv import VizDoomTrain
+from vizdoomEnv import VizDoomGym
 from callback import TrainCallback
 from matplotlib import pyplot as plt
 from stable_baselines3.common import env_checker
-from stable_baselines3 import A2C
+from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.monitor import Monitor
 from packaging import version
 
 SCENARIO = 'deadly_corridor'
 LOG_DIR = 'logs/' + SCENARIO
-TOTAL_TIMESTEPS = 1e6
-print(sys.path)
-env = VizDoomTrain(SCENARIO)
+TOTAL_TIMESTEPS = 500000
+
+env = VizDoomGym(SCENARIO)
 env = Monitor(env)
-model = A2C('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, learning_rate=0.0001, n_steps=2048)
+model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=0.00001, n_steps=4096)
 logger = configure(LOG_DIR, ["stdout", "csv", "tensorboard"])
 model.set_logger(logger)
 callback = TrainCallback(10000, LOG_DIR)
-model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=1) #decrease frequency of output with log_interval
+model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback) #decrease frequency of output with log_interval
 
