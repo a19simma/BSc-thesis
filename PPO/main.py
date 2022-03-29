7e7d05a69615e27d709e778de0a3a4f102ac2b2e
e15marol <markus-olsson@outlook.com>
2022-03-29 12:30:10 +0200

    Reverted because there was no data output

--- PPO/main.py
+++ PPO/main.py
@@ -1,102 +1,19 @@
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-#sys.path.append('../')
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from vizdoomEnv import VizDoomTrain
+import os, sys
+sys.path.append('../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from vizdoomEnv import VizDoomGym
 from callback import TrainCallback
-from stable_baselines3 import DQN, A2C, PPO
+from matplotlib import pyplot as plt
+from stable_baselines3.common import env_checker
+from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.evaluation import evaluate_policy
-import optuna
-
+from packaging import version
 
 SCENARIO = 'deadly_corridor'
-TOTAL_TIMESTEPS = 30e5 #3 Miljoner
-ALGORITHM = "PPO"
-STUDY_NAME = SCENARIO + "_" + ALGORITHM
-LOG_DIR = 'logs/' + STUDY_NAME
-
-# Defaults are taken from the 2013 Nature paper.  https://arxiv.org/abs/1312.5602
-
-
-def optimize_params(trial):
-    return{
-        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-7, 1),
-        # size of the buffer was reduced because of ram limitations.
-        'buffer_size':  int(1e5),
-        'learning_starts': TOTAL_TIMESTEPS/20,
-        'batch_size': trial.suggest_int('batch_size', 1, 128),
-        'tau': trial.suggest_float('tau', 0, 1.0),
-        'gamma': trial.suggest_float('gamma', 0, 1.0),
-        'train_freq': trial.suggest_int('train_freq', 1, 2048),
-        'gradient_steps': trial.suggest_int('gradient_steps', 1, 10),
-    }
-
-
-def optimize_agent(trial):
-    model_params = optimize_params(trial)
-    RUN_NAME = 'Trial_' + str(trial.number) + '_'
-    for key in model_params:
-        RUN_NAME += key + '=' + str(model_params[key]) + '_'
-        RUN_NAME = RUN_NAME[:-1]
-
-    env = VizDoomTrain(SCENARIO)
-    env = Monitor(env)
-    #model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, **model_params)
-    model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, learning_rate=0.00001, n_steps=4096)
-    logger = configure(LOG_DIR + '/' + RUN_NAME,
-                       ["stdout", "csv", "tensorboard"])
-    model.set_logger(logger)
-    callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
-    # decrease frequency of output with log_interval
-    model.learn(total_timesteps=TOTAL_TIMESTEPS,
-                callback=callback, log_interval=256)
-    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
-    return mean_reward
-
-
-if __name__ == '__main__':
-    # for the distributed solution a mysql server is needed with a database names optuna
-    study = optuna.create_study(direction='maximize', study_name=STUDY_NAME,
-                                storage="mysql://root@localhost/optuna", load_if_exists=True)
-    try:
-        study.optimize(optimize_agent, n_trials=1, gc_after_trial=True)
-    except KeyboardInterrupt:
-        print('Interrupted by keyboard')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-SCENARIO = 'deadly_corridor'
-TOTAL_TIMESTEPS = 5e5
-ALGORITHM = "DQN"
-STUDY_NAME = SCENARIO + "_" + ALGORITHM
-LOG_DIR = 'logs/' + STUDY_NAME
+LOG_DIR = 'logs/' + SCENARIO
+TOTAL_TIMESTEPS = 500000
 
 env = VizDoomGym(SCENARIO)
 env = Monitor(env)
@@ -104,8 +21,4 @@ model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=
 logger = configure(LOG_DIR, ["stdout", "csv", "tensorboard"])
 model.set_logger(logger)
 callback = TrainCallback(10000, LOG_DIR)
-model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback, log_interval=256) #decrease frequency of output with log_interval
-
-
-
-'''
+model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=callback) #decrease frequency of output with log_interval
---
+++ PPO/vizdoomEnv.py
@@ -0,0 +1,103 @@
+
+# Import vizdoom for game env
+from vizdoom import * 
+# Import random for action sampling
+import random
+# Import time for sleeping
+import time 
+# Import numpy for identity matrix
+import numpy as np
+# Import environment base class from OpenAI Gym
+from gym import Env
+# Import gym spaces 
+from gym.spaces import Discrete, Box
+# Import opencv 
+import cv2
+
+# Create Vizdoom OpenAI Gym Environment
+class VizDoomGym(Env): 
+    # Function that is called when we start the env
+    
+    def __init__(self, render=True, config='deadly_corridor.cfg'): 
+        # Inherit from Env
+        super().__init__()
+        # Setup the game 
+        self.game = DoomGame() #type: ignore
+        self.game.load_config(config)
+        
+        # Render frame logic
+        if render == False: 
+            self.game.set_window_visible(False)
+        else:
+            self.game.set_window_visible(True)
+        
+        # Start the game 
+        self.game.init()
+        
+        # Create the action space and observation space
+        self.observation_space = Box(low=0, high=255, shape=(100,160,1), dtype=np.uint8) 
+        #self.action_space = Discrete(7)
+        self.action_space = Discrete(self.game.get_available_buttons_size())
+
+        # Game variables: HEALTH DAMAGE_TAKEN HITCOUNT SELECTED_WEAPON_AMMO
+        self.damage_taken = 0
+        self.hitcount = 0
+        self.ammo = 26 ## CHANGED
+        
+        
+    # This is how we take a step in the environment
+    def step(self, action):
+        # Specify action and take step 
+        actions = np.identity(7)
+        movement_reward = self.game.make_action(actions[action], 4) 
+        
+        reward = 0 
+        # Get all the other stuff we need to retun 
+        if self.game.get_state(): 
+            state = self.game.get_state().screen_buffer
+            state = self.grayscale(state)
+            
+            # Reward shaping
+            game_variables = self.game.get_state().game_variables
+            health, damage_taken, hitcount, ammo = game_variables
+            
+            # Calculate reward deltas
+            damage_taken_delta = -damage_taken + self.damage_taken
+            self.damage_taken = damage_taken
+            hitcount_delta = hitcount - self.hitcount
+            self.hitcount = hitcount
+            ammo_delta = ammo - self.ammo
+            self.ammo = ammo
+            
+            reward = movement_reward + damage_taken_delta*5 + hitcount_delta*50  + ammo_delta*5
+            info = ammo
+        else: 
+            state = np.zeros(self.observation_space.shape)
+            info = 0 
+        
+        info = {"info":info}
+        done = self.game.is_episode_finished()
+        
+        return state, reward, done, info 
+    
+    # Define how to render the game or environment 
+    def render(): 
+        pass
+    
+    # What happens when we start a new game 
+    def reset(self): 
+        self.game.new_episode()
+        state = self.game.get_state().screen_buffer
+        return self.grayscale(state)
+    
+    # Grayscale the game frame and resize it 
+    def grayscale(self, observation):
+        gray = cv2.cvtColor(np.moveaxis(observation, 0, -1), cv2.COLOR_BGR2GRAY)
+        resize = cv2.resize(gray, (160,100), interpolation=cv2.INTER_CUBIC)
+        state = np.reshape(resize, (100,160,1))
+        return state
+    
+    # Call to close down the game
+    def close(self): 
+        self.game.close()
+    
