e42af996b956efd539f5b0b9f3547b8aef7cad6b
a19simma <a19simma@student.his.se>
2022-04-05 12:17:19 +0200

    reward shaping

--- vizdoomEnv.py
+++ vizdoomEnv.py
@@ -18,13 +18,13 @@ class VizDoomTrain(Env):
         self.game.set_window_visible(visible)
         self.game.set_screen_format(ScreenFormat.GRAY8) #type: ignore
         self.game.set_screen_resolution(ScreenResolution.RES_160X120) #type: ignore
-        self.game.set_available_game_variables([GameVariable.SELECTED_WEAPON_AMMO, GameVariable.DAMAGE_TAKEN, GameVariable.HITCOUNT])  # type: ignore
+        self.game.set_available_game_variables([GameVariable.SELECTED_WEAPON_AMMO, GameVariable.DAMAGE_TAKEN, GameVariable.KILLCOUNT])  # type: ignore
         self.game.set_doom_skill(3)
         self.game.init()
         self.damagecount = 0
         self.ammo = self.game.get_state().game_variables[0]
         self.damage_taken = 0
-        self.hitcount = 0
+        self.killcount_delta = 0
         self.observation_space = Box(low=0, high=255, shape=(1, 120, 160), dtype=np.uint8)
         self.action_space = Discrete(self.game.get_available_buttons_size())
 
@@ -37,15 +37,15 @@ class VizDoomTrain(Env):
         if self.game.get_state(): 
             state = self.game.get_state().screen_buffer
             game_variables = self.game.get_state().game_variables
-            damage_taken, hitcount, ammo = game_variables
+            damage_taken, killcount_delta, ammo = game_variables
             # Calculate reward deltas
             damage_taken_delta = -damage_taken + self.damage_taken
             self.damage_taken = damage_taken
-            hitcount_delta = hitcount - self.hitcount
-            self.hitcount = hitcount
+            killcount_delta = killcount_delta - self.killcount_delta
+            self.killcount_delta = killcount_delta
             ammo_delta = ammo - self.ammo
             self.ammo = ammo
-            reward = movement_reward + damage_taken_delta*5 + hitcount_delta*50  + ammo_delta*5
+            reward = (movement_reward/10 + damage_taken_delta*10 + killcount_delta*100  + ammo_delta*5)/1000
         else:
             state = np.zeros(self.observation_space.shape)
         info = {"info":0}
