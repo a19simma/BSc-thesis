b2b679d450d19491004ea50e570b98f3eaa4d99c
Kasper Witt <kasper.m.witt@outlook.com>
2022-04-12 00:29:54 +0200

    Update demo.py

--- A2C/demo.py
+++ A2C/demo.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from vizdoomEnvPaperDivided import VizDoomTrain
+from vizdoomEnv import VizDoomTrain
 from stable_baselines3 import DQN, A2C, PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
