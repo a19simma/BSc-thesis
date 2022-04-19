e31b3a2973f4348223ad85857d4c38fa245c976b
a19simma <a19simma@student.his.se>
2022-04-19 12:50:15 +0200

    more fixes

--- DQN/optimized.py
+++ DQN/optimized.py
@@ -14,7 +14,6 @@ import sqlcon  # use this to save connection details for the RDB
 # make_action which takes a list of button states given by an array of 0 or 1 with the
 # length of the number of buttons.
 
-
 SCENARIO = 'deadly_corridor_optimized'
 TOTAL_TIMESTEPS = 1e6
 ALGORITHM = "DQN"
@@ -26,7 +25,7 @@ LOG_DIR = 'logs/' + STUDY_NAME
 # Buffersize and learning starts were reduced compared to the original study to reflect the 
 # lower total timesteps
 
-def params(trial):
+def optimize_params(trial):
     return{
         'learning_rate': 1.9070714918537712e-05, 
         # size of the buffer was reduced because of ram limitations.
@@ -38,20 +37,33 @@ def params(trial):
         'train_freq': 3, 
         'gradient_steps': 10,
     }
-model_params = params()
-RUN_NAME = 'Default_' + str(1) + '_'
-for key in model_params:
-    RUN_NAME += key + '=' + str(model_params[key]) + '_'
-    RUN_NAME = RUN_NAME[:-1]
+def optimize_agent(trial):
+    model_params = optimize_params(trial)
+    RUN_NAME = 'Trial_' + str(trial.number) + '_'
+    for key in model_params:
+        RUN_NAME += key + '=' + str(model_params[key]) + '_'
+        RUN_NAME = RUN_NAME[:-1]
 
-env = VizDoomTrain(SCENARIO)
-env = Monitor(env)
-model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR,
-            verbose=0, **model_params)
-logger = configure(LOG_DIR + '/' + RUN_NAME,
-                    ["stdout", "csv", "tensorboard"])
-model.set_logger(logger)
-callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
-# decrease frequency of output with log_interval
-model.learn(total_timesteps=TOTAL_TIMESTEPS,
-            callback=callback, log_interval=20)
+    env = VizDoomTrain(SCENARIO)
+    env = Monitor(env)
+    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR,
+                verbose=0, **model_params)
+    logger = configure(LOG_DIR + '/' + RUN_NAME,
+                       ["stdout", "csv", "tensorboard"])
+    model.set_logger(logger)
+    callback = TrainCallback(10000, LOG_DIR + '/' + RUN_NAME)
+    # decrease frequency of output with log_interval
+    model.learn(total_timesteps=TOTAL_TIMESTEPS,
+                callback=callback, log_interval=20)
+    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
+    return mean_reward
+
+
+if __name__ == '__main__':
+    # for the distributed solution a mysql server is needed with a database names optuna
+    study = optuna.create_study(direction='maximize', study_name=STUDY_NAME,
+                                storage=sqlcon.con, load_if_exists=True)
+    try:
+        study.optimize(optimize_agent, n_trials=1, gc_after_trial=True)
+    except KeyboardInterrupt:
+        print('Interrupted by keyboard')
