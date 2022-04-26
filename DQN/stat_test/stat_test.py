279c58b059b936fdfe595a3cc2c620bf42fcb96e
a19simma <a19simma@student.his.se>
2022-04-26 14:43:58 +0200

    added cached csv files for the sample output and ability to load them instead of redrawing samples

--- DQN/stat_test/stat_test.py
+++ DQN/stat_test/stat_test.py
@@ -13,16 +13,25 @@ model_dir = os.path.dirname(os.path.abspath(__file__))
 optimized_model = DQN.load(model_dir + "\optimized_model.zip")
 default_model = DQN.load(model_dir + "\default_model.zip")
 
-data_optimized = []
-data_default = []
+if not os.path.exists(model_dir + '\optimized_evaluation_sample.csv'):
+    data_optimized = []    
+    for n in range(50):
+        mean_reward, _ = evaluate_policy(optimized_model, env, n_eval_episodes=1)
+        data_optimized.append(mean_reward)
+    data_optimized = np.array(data_optimized) 
+    np.savetxt('optimized_evaluation_sample.csv', data_optimized)
+else: 
+    data_optimized = np.loadtxt('optimized_evaluation_sample.csv')
 
-for n in range(50):
-    mean_reward, _ = evaluate_policy(optimized_model, env, n_eval_episodes=1)
-    data_optimized.append(mean_reward)
-
-for n in range(50):
-    mean_reward, _ = evaluate_policy(default_model, env, n_eval_episodes=1)
-    data_default.append(mean_reward)
+if not os.path.exists(model_dir + '\default_evaluation_sample.csv'):
+    data_default = []   
+    for n in range(50):
+        mean_reward, _ = evaluate_policy(default_model, env, n_eval_episodes=1)
+        data_default.append(mean_reward)
+    data_default = np.array(data_default) 
+    np.savetxt('default_evaluation_sample.csv', data_optimized)
+else: 
+    data_default = np.loadtxt('default_evaluation_sample.csv')
 
 statistic, pvalue = mannwhitneyu(data_default,data_optimized)
 
